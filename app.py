b4986e236ff89d95f1e0dac95593b832d1c10ac8
madycakes <viclee02@gmail.com>
2018-09-06 19:45:15 -1000

    add print function

--- app.py
+++ app.py
@@ -33,7 +33,9 @@ west_side = ['Tupac', 'Dre', 'Snoop']
 
 haters = ['Keyshia Cole', 'Wendy Williams', '50 Cent', 'Lil Kim']
 
+# 13. Create a function that will print that last digit of an integer. 
 
+print()
 
 
 
